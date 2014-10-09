0410a386c943faf0d4d36142d2c5cd1ae3c2b4ad
mroque <mpc4mirl@gmail.com>
2014-10-09 17:27:20 -0500

    Corrección multimedia descripción

--- application/models/multimedia_model.php
+++ application/models/multimedia_model.php
@@ -19,7 +19,7 @@ class multimedia_model extends CI_Model
 
 	public function get_multimedia_byid($multimedia_id)
 	{
-		$query = $this ->db->query ('select id,titulo,autor,descripcion,anio,genero,img,tipo from get_lista_multimedia_all where id='.$multimedia_id);
+		$query = $this ->db->query ('select id,titulo,autor,descripcion,anio,genero,img,tipo from get_lista_multimedia_all where id='.$multimedia_id.' limit 1');
 		return $query -> result_array();
 	}
 }
