26dea86611a6665f8156eab3c2354f6cd8d443e2
mroque <mpc4mirl@gmail.com>
2014-11-03 18:09:46 -0500

    Multimedia

--- application/models/multimedia_model.php
+++ application/models/multimedia_model.php
@@ -13,7 +13,7 @@ class multimedia_model extends CI_Model
 
 	public function get_lista_multimedia_bycualidadtipo($cualidad_id,$tipo_id)
 	{
-		$query = $this ->db->query ('select id,titulo,genero,img from get_lista_multimedia_cualidad_all where cualidad='.$cualidad_id.' and tipo='.$tipo_id);
+		$query = $this ->db->query ('select id,titulo,anio,autor,img from get_lista_multimedia_cualidad_all where cualidad='.$cualidad_id.' and tipo='.$tipo_id);
 		return $query -> result_array();
 	}
 
